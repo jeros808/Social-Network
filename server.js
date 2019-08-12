1d46b82e404bdd38d94d675d595426b04c788842
Jeff Rosal <jeros808@aol.com>
2019-08-12 13:06:41 -0500

    add to production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const app = express();
+const path = require;
+const app = express("path");
 
 //connect database
 connectDB();
@@ -8,13 +9,24 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running"));
+//get rid of for production-> app.get("/", (req, res) => res.send("API running"));
 
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
